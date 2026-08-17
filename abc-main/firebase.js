e428d68c867d783ffc2156f9af24350722f9a1ad
hai2xx2h-pixel <hai2xx2h@gmail.com>
2026-08-17 13:56:52 +0700

    Update firebase.js

--- abc-main/firebase.js
+++ abc-main/firebase.js
@@ -1,31 +1,49 @@
 import { initializeApp } from
-    "https://www.gstatic.com/firebasejs/12.16.0/firebase-app.js";
+    "https://www.gstatic.com/firebasejs/11.10.0/firebase-app.js";
 
-import { getAuth } from
-    "https://www.gstatic.com/firebasejs/12.16.0/firebase-auth.js";
+import { getAuth, signInAnonymously } from
+    "https://www.gstatic.com/firebasejs/11.10.0/firebase-auth.js";
 
 import { getFirestore } from
-    "https://www.gstatic.com/firebasejs/12.16.0/firebase-firestore.js";
+    "https://www.gstatic.com/firebasejs/11.10.0/firebase-firestore.js";
 
 
-// 🎯 Dán thông số thực tế của bạn vào đây:
 const firebaseConfig = {
-    apiKey: "AIzaSyDqVgpx9OSwEl_NQIR-KflGD-B0FIysZhc",
-    authDomain: "du-an-cua-hai.firebaseapp.com",
-    projectId: "du-an-cua-hai",
-    storageBucket: "du-an-cua-hai.firebasestorage.app",
-    messagingSenderId: "924184286263",
-    appId: "1:924184286263:web:3d7b6b94e74fef87585bb2",
-    measurementId: "G-8DZLWTWTKW"
+
+    apiKey: "DAN_API_KEY_CUA_BAN",
+
+    authDomain:
+        "DAN_PROJECT_ID.firebaseapp.com",
+
+    projectId:
+        "DAN_PROJECT_ID",
+
+    storageBucket:
+        "DAN_PROJECT_ID.firebasestorage.app",
+
+    messagingSenderId:
+        "DAN_MESSAGING_SENDER_ID",
+
+    appId:
+        "DAN_APP_ID"
 };
 
 
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const db = getFirestore(app);
+const app =
+    initializeApp(firebaseConfig);
+
+
+const auth =
+    getAuth(app);
+
+
+const db =
+    getFirestore(app);
+
 
 export {
     app,
     auth,
-    db
-};firebase.js
+    db,
+    signInAnonymously
+};
